218c7ac6da79996b85fbda16705d1f966aaa13a6
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-03 15:48:41 -0300

    Criando Projeto de Teste xUnit

---
+++ Tester.XUnit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tester.XUnit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
