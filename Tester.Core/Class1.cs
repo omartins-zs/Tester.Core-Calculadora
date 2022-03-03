1d614cabad7a4a8a418906103ae508789507c520
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-03 15:56:03 -0300

    Criando Operação de Soma

--- Tester.Core/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Tester.Core
-{
-    public class Class1
-    {
-        // Aqui fica o codigo para o teste
-    }
-}
---
+++ Tester.Core/Operacoes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tester.Core
+{
+    public class Operacoes
+    {
+        public static double Somar(double pNum, double sNum)
+        {
+            return pNum + sNum;
+        }
+    }
+}
