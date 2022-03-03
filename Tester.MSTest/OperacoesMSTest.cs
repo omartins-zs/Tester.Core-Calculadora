51451b146600c47c7f123f579dcd4c8775b4e2ed
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-03 16:08:17 -0300

    Criado Codigo de Teste do MSTest

---
+++ Tester.MSTest/OperacoesMSTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tester.Core;
+
+namespace Tester.MSTest
+{
+    // classe de testes que você queira executar
+    [TestClass]
+    public
+    class OperacoesMSTest
+    {
+        // método de teste
+        [TestMethod]
+        // descricao do teste
+        public
+        void SomarDoisNumeros_RetornaResultado()
+        {
+            // arrange organizar, preparar o teste
+            double primeiroNumero = 10;
+            double segundoNumero = 20;
+            // act agir execução/ chamada do método
+            var resultado = Operacoes.Somar(primeiroNumero, segundoNumero);
+            // Assert comportamento esperado, comportamento obtido
+            Assert.AreEqual(30, resultado);
+        }
+    }
+}
