38d71f80290a432549ba656842acbe1673acb118
omartins-zs <gabrielmatheus209@gmail.com>
2022-03-03 16:49:08 -0300

    Criando Teste para Classe Conta

---
+++ Tester.Core/Conta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester.Core
+{
+    public class Conta
+    {
+        public double Saldo { get; private set; }
+        public Conta(double saldo)
+        {
+            Saldo = saldo;
+        }
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+    }
+}
